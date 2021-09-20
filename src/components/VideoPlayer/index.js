14676054d5576ce3fdc03b954789b51b8492338d
root <abacusrehan@gmail.com>
2021-09-20 10:52:50 +0000

    initial Commit

---
+++ src/components/VideoPlayer/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+import ReactPlayer from 'react-player'
+
+const VideoPlayer = () => (
+  <div className="video-container">
+    <h1 className="heading">Video Player</h1>
+    <div className="responsive-container" />
+  </div>
+)
+
+export default VideoPlayer
